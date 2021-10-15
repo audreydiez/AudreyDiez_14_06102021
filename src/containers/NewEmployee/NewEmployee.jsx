e410ff9f7dcbfcaf9abe3e5c80cdf1b28c762852
Audrey Diez <audrey.diez@gmail.com>
2021-10-15 16:03:28 +0200

    feat: Styling datepicker

--- src/containers/NewEmployee/NewEmployee.jsx
+++ src/containers/NewEmployee/NewEmployee.jsx
@@ -4,6 +4,9 @@ import HeaderTitle from '../../components/HeaderTitle/HeaderTitle'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
+import { getMonth, getYear } from 'date-fns'
+import range from 'lodash/range'
+
 import Select from 'react-select'
 import { useState } from 'react'
 
@@ -15,6 +18,21 @@ function NewEmployee() {
     ]
 
     const [startDate, setStartDate] = useState(new Date())
+    const years = range(1990, getYear(new Date()) + 1, 1)
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ]
 
     return (
         <div className="new-employee">
@@ -60,6 +78,55 @@ function NewEmployee() {
                                 Start date
                             </label>
                             <DatePicker
+                                renderCustomHeader={({
+                                    date,
+                                    changeYear,
+                                    changeMonth,
+                                    decreaseMonth,
+                                    increaseMonth,
+                                    prevMonthButtonDisabled,
+                                    nextMonthButtonDisabled
+                                }) => (
+                                    <div
+                                        style={{
+                                            margin: 10,
+                                            display: 'flex',
+                                            justifyContent: 'center'
+                                        }}>
+                                        <button
+                                            onClick={decreaseMonth}
+                                            disabled={prevMonthButtonDisabled}>
+                                            {'<'}
+                                        </button>
+                                        <select
+                                            value={getYear(date)}
+                                            onChange={({ target: { value } }) => changeYear(value)}>
+                                            {years.map((option) => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </select>
+
+                                        <select
+                                            value={months[getMonth(date)]}
+                                            onChange={({ target: { value } }) =>
+                                                changeMonth(months.indexOf(value))
+                                            }>
+                                            {months.map((option) => (
+                                                <option key={option} value={option}>
+                                                    {option}
+                                                </option>
+                                            ))}
+                                        </select>
+
+                                        <button
+                                            onClick={increaseMonth}
+                                            disabled={nextMonthButtonDisabled}>
+                                            {'>'}
+                                        </button>
+                                    </div>
+                                )}
                                 selected={startDate}
                                 onChange={(date) => setStartDate(date)}
                             />
